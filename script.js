49294dcabd8a6fa6ada83d667f93cb9b66beffdb
bhargaviRdy <46837526+bhargaviRdy@users.noreply.github.com>
2021-09-30 01:11:01 +0530

    update readme

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const notes = [2000, 500, 200, 100, 50, 20, 10 , 5, 1];
 
 
 btn.addEventListener("click", function returnChange(){
-    console.log(billAmount.value,cashGiven.value);
-    if(cashGiven.value > 0 && billAmount.value > 0 && cashGiven.value >= billAmount.value){
+    // console.log(billAmount.value,cashGiven.value);
+    if(Number(cashGiven.value) > 0 && Number(billAmount.value) > 0 && Number(cashGiven.value) >= Number(billAmount.value)){
+        console.log(billAmount.value,cashGiven.value);
         errorSpan.innerText = "";
         var amountToReturn = cashGiven.value-billAmount.value;
+        console.log(amountToReturn);
 
         notes.map(note => {
+            document.getElementById(note).innerHTML = "0";
             var toReturn =  Math.floor(amountToReturn/note);
             if(toReturn >= 1){
                 document.getElementById(note).innerHTML = toReturn;
@@ -21,7 +24,11 @@ btn.addEventListener("click", function returnChange(){
         })
     }
     else{
-        errorSpan.innerText = "cash given and bill amount must be > 0  and cash given should be > bill amount"
+        errorSpan.innerText = "cash given and bill amount must be > 0  and cash given should be > bill amount";
+        notes.map(note => {
+            document.getElementById(note).innerHTML = "0";
+        });
+        
     }
 });
 
